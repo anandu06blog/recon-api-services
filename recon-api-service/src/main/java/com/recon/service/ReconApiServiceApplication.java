6132ef4b424be79b33a817892d9d3d4e9928a0ec
anandu06blog <113542195+anandu06blog@users.noreply.github.com>
2025-05-26 12:18:06 +0530

    Initial Spring Setup

---
+++ recon-api-service/src/main/java/com/recon/service/ReconApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recon.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconApiServiceApplication.class, args);
+	}
+
+}
